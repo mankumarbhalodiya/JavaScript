0a0d20f883ef179553e31536661cc7efe0afd24a
Man Bhalodiya <mankumar.bhalodiya130005@marwadiuniversity.ac.in>
2026-06-11 14:13:44 +0530

    Delete Basic_Of_Laravel directory

--- Basic_Of_Laravel/HostelManagement/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HostelController;
-
-Route::get('/add-hostel', [HostelController::class, 'addData']);
-Route::get('/show-hostel', [HostelController::class, 'showData']);
-Route::get('/show-hostel', [HostelController::class, 'showData']);
